2252cd3ec09e58333bf02d79047130fe2b069e0c
root <sehee.lee.kr@gmail.com>
2020-07-30 22:28:57 +0900

    For validation test

--- gpio/gpio_module.c
+++ gpio/gpio_module.c
@@ -2,30 +2,33 @@
 #include <linux/cdev.h>
 #include <linux/module.h>
 #include <linux/io.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
+//#include <asm/uaccess.h>
 //#include <mach/platform.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sehee Lee");
 MODULE_DESCRIPTION("RASPBERRY PI GPIO MODULE");
+MODULE_INFO(intree, "Y");
 
-#define BCM_IO_BASE	0x3F00000
+#define BCM_IO_BASE	0xFE000000
 #define	GPIO_BASE	(BCM_IO_BASE + 0x200000)
 
 #define GPIO_SIZE	(256)
 #define GPIO_IN(g)	(*(gpio + (g/10)) &= ~(7 << ((g%10)*3)))
 #define GPIO_OUT(g)	(*(gpio + (g/10)) &= 1 << ((g%10)*3))
 
-#define GPIO_SET(g)	(*(gpio + 7) = (1 << g));
-#define GPIO_CLR(g)	(*(gpio +10) = (1 << g));
-#define GPIO_GET(g)	(*(gpio +13) & (1 << g));
+#define GPIO_SET(g)	(*(gpio + 7) = (1 << g))
+#define GPIO_CLR(g)	(*(gpio +10) = (1 << g))
+#define GPIO_GET(g)	(*(gpio +13) & (1 << g))
+
 #define GPIO_MAJOR	200
 #define	GPIO_MINOR	0
 #define	GPIO_DEVICE	"gpioled"
 #define GPIO_LED  18
 
 volatile unsigned *gpio;
-static char mag[BLOCK_SIZE] = {0};
+static char msg[BLOCK_SIZE] = {0};
 //static char mag[80] = {0,};
 
 static int gpio_open(struct inode*, struct file*);
@@ -62,7 +65,7 @@ int init_modules(void)
 	gpio_cdev.owner = THIS_MODULE;
 
 	count = 1;
-	err = cdev_add(&gpio_cdev,devno, count);
+	err = cdev_add(&gpio_cdev, devno, count);
 	if(err <0){
 		printk("ERROR :Device Add\n");
 		return -1;
@@ -71,17 +74,18 @@ int init_modules(void)
 	printk("'mkmod /dev/%s c %d 0'\n", GPIO_DEVICE, GPIO_MAJOR);
 	printk("'chmod 666 /dev/%s'\n", GPIO_DEVICE);
 
-	map = ioremap(GPIO_BASE, GPIO_SIZE);
+//	map = ioremap(GPIO_BASE, GPIO_SIZE);
 
-	if(!map){
-		printk("ERROR :mapping GPIO memory\n");
-		return -EBUSY;
-	}
+//	if(!map){
+//		printk("ERROR :mapping GPIO memory\n");
+//		iounmap(map);
+//		return -EBUSY;
+//	}
 
-	gpio = (volatile unsigned int*)map;
+//	gpio = (volatile unsigned int*)map;
 
-	GPIO_IN(GPIO_LED);
-	GPIO_OUT(GPIO_LED);
+//	GPIO_IN(GPIO_LED);
+//	GPIO_OUT(GPIO_LED);
 
 	return 0;
 }
@@ -93,9 +97,9 @@ void cleanup_modules(void)
 
 	cdev_del(&gpio_cdev);
 
-	if(gpio){
-		iounmap(gpio);
-	}
+//	if(gpio){
+//		iounmap(gpio);
+//	}
 
 	module_put(THIS_MODULE);
 
@@ -113,20 +117,31 @@ static int gpio_open(struct inode* inode, struct file* file)
 	return 0;
 }
 
-static ssize_t gpio_read(struct file* inode, char* buff, size_t len, loff_t* off)
+static ssize_t gpio_read(struct file* inode, char* buf, size_t len, loff_t* off)
 {
-	return 0;
+	int count;
+	strcat(msg, "from Kernel");
+	count = copy_to_user(buf, msg, strlen(msg)+1);
+	//count = raw_copy_to_user(buf, msg, strlen(msg)+1);
+	return count;
 }
 
 static ssize_t gpio_write(struct file* file, const char* buf, size_t len, loff_t* off) 
 {
+	short count;
+	memset(msg, 0, BLOCK_SIZE);
+	count = copy_from_user(msg, buf, len);
+	//count = raw_copy_from_user(msg, buf, len);
 
-	return 0;
+//	(!strcmp(msg, "0")) ? GPIO_CLR(GPIO_LED) : GPIO_SET(GPIO_LED);
+	printk("GPIO Device write %s(%d)\n", msg, len);
+
+	return count;
 }
 static int gpio_close(struct inode* inode, struct file* file)
 {
-	return 0;
 
+	return 0;
 }
 
 
---
+++ gpio/led.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	char buf[80];
+
+	char i = 0;
+	int fd = -1;
+	memset(buf, 0, 80);
+
+	printf("GPIO SET : %s\n", argv[1]);
+	fd = open ("dev/gpioled", O_RDWR);
+	if (fd ==-1){
+		printf("fail to open dev/gpioled\n");
+	}
+
+	write(fd, argv[1], strlen(argv[1]), NULL);
+
+	read(fd, buf, strlen(argv[1]), NULL);
+	printf("Read data : %s\n",buf);
+
+	close(fd);
+
+	return 0;
+}
